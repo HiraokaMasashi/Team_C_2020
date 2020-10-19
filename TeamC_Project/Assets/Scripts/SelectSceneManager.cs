80609245ef669e0bba85e2f7f37ab9d6ba6e23a4
後潟 斗志朗 <gensenya@gmail.com>
2020-10-19 17:10:52 +0900

    タイトル⇔セレクト→メイン1のシーン遷移(シーンのデザインは未定)

---
+++ TeamC_Project/Assets/Scripts/SelectSceneManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image[] scenes;
+    [SerializeField]
+    private int selectNumber;
+    private int length;
+    [SerializeField]
+    private float interval; //カーソル移動のインターバルフレーム
+    private float timer;
+    private float eTimer;
+
+    [SerializeField]
+    private InputManager Input;
+    [SerializeField]
+    private FadeScene sceneManager;
+
+    [SerializeField]
+    private string[] sceneNames;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        length = scenes.Length - 1;
+        selectNumber = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer++;
+        float v = -Input.GetL_Stick_Vertical();
+        if(timer >= interval && v != 0)
+        {
+            selectNumber += (int)(1 * (v/Mathf.Abs(v)));
+            selectNumber = (0 < selectNumber) ? selectNumber : 0;
+            selectNumber = (length > selectNumber) ? selectNumber : length;
+            timer = 0;
+        }
+        if (v == 0)
+            timer = interval;
+
+        //Aかメニューボタンで選択中のシーンへ
+        if (Input.GetA_ButtonDown() || Input.GetMenu_ButtonDown())
+            sceneManager.ChangeNextScene(sceneNames[selectNumber]);
+
+        //Bボタンでタイトルへ
+        if(Input.GetB_ButtonDown())
+            sceneManager.ChangeNextScene("Title");
+
+        ActiveButton(selectNumber);
+    }
+
+    //選択中のボタンの演出を書く
+    private void ActiveButton(int number)
+    {
+        eTimer++;
+        foreach (var s in scenes)
+            s.color = Color.white;
+        if (eTimer % 10 == 0)
+            scenes[number].color = Color.white;
+        else
+            scenes[number].color = Color.yellow;
+    }
+}
---
+++ TeamC_Project/Assets/Scripts/TitleSceneManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform titleLogo;
+    [SerializeField]
+    private Text pressLogo;
+    private bool moveEnd;
+    [SerializeField]
+    private InputManager Input;
+    [SerializeField]
+    private FadeScene sceneManager;
+
+    void Awake()
+    {
+        pressLogo.gameObject.SetActive(false);
+        titleLogo.transform.position += Vector3.up * 760;
+        moveEnd = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ムービー再生
+        if (!moveEnd)
+        {
+            //ボタンが押されたらムービー終了
+            if (PressAnyButton())
+            {
+                moveEnd = true;
+                titleLogo.anchoredPosition = new Vector3(titleLogo.anchoredPosition.x, 100);
+                pressLogo.gameObject.SetActive(true);
+            }
+            Opening();
+            return;
+        }
+        else
+        {
+            if ((int)Time.time % 2 == 0)
+                pressLogo.gameObject.SetActive(false);
+            else
+                pressLogo.gameObject.SetActive(true);
+
+            //ムービー終了後にボタンを押すと次のシーンへ
+            if (PressAnyButton())
+                sceneManager.ChangeNextScene("Select");
+        }
+    }
+
+    //タイトルで流すムービー演出を書く
+    private void Opening()
+    {
+        //タイトルロゴを動かす
+        titleLogo.position -= Vector3.up * 2;
+
+        //ロゴが指定の位置に来たら終了
+        if (titleLogo.anchoredPosition.y <= 100)
+        {
+            titleLogo.anchoredPosition = new Vector3(titleLogo.anchoredPosition.x, 100);
+            pressLogo.gameObject.SetActive(true);
+            moveEnd = true;
+        }
+    }
+
+    //複数のボタンの入力を受け付ける
+    private bool PressAnyButton()
+    {
+        if (Input.GetA_ButtonDown() || Input.GetB_ButtonDown()
+        || Input.GetX_ButtonDown() || Input.GetY_ButtonDown()
+        || Input.GetMenu_ButtonDown() || Input.GetView_ButtonDown())
+            return true;
+        else
+            return false;
+    }
+}
