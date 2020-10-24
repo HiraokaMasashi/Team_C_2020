a70f9ebb4fd4e375e5a13a1aad13dbf6f2b485e5
kamoi <kamokichi0930@ezweb.ne.jp>
2020-10-24 20:11:05 +0900

    PauseManager.prefabを追加

---
+++ TeamC_Project/Assets/Scripts/TestObjMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObjMove : MonoBehaviour
+{
+    float xv;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xv = 1;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0, 1, 0));
+    }
+}
