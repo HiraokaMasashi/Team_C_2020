6e13098be136f1b895fee973fe97f75793de51fd
後潟 斗志朗 <gensenya@gmail.com>
2020-10-27 22:43:50 +0900

    Wave処理

--- TeamC_Project/Assets/Scripts/Enemy/EnemyManager.cs
+++ TeamC_Project/Assets/Scripts/Enemy/EnemyManager.cs
@@ -13,33 +13,69 @@ public class EnemyManager : MonoBehaviour
     private float time = 0f;
     //敵の生成パターン
     public Transform[] instancePattern;
+    //現在Waveの敵
+    private GameObject[] waveEnemeis;
+    //Wave数
+    [SerializeField]
+    private int maxWave;
+    //現在のWave
+    private int wave;
+    //Waveエンドフラグ
+    public bool isEnd;
 
     // Start is called before the first frame update
     void Start()
     {
         //時間間隔
         interval = 5f;
+        wave = 0;
+        StartCoroutine(Instance(wave));
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
+        /*
+           time += Time.deltaTime;
         //経過時間が生成時間になったとき(生成時間より大きくなったとき)
         if (time > interval)
         {
-            Instance(0);
             //enemyをインスタンス化する(生成する)
-            //   GameObject enemy = Instantiate(enemyPrefab);
+               GameObject enemy = Instantiate(enemyPrefab);
             //生成した敵の座標を決定する(現状　このオブジェクトの座標に生成)
-            //   enemy.transform.position = transform.position;
+               enemy.transform.position = transform.position;
             //経過時間を初期化して再度時間計測を始める
             time = 0f;
+        }
+        */
+
+
+        if (waveEnemeis != null)
+        {
+            int count = 0;
+            //現在残っている敵の数をカウント
+            foreach (var e in waveEnemeis)
+            {
+                if (e == null)
+                    count++;
+            }
+            //敵が全滅したらWaveを進める
+            if (count == waveEnemeis.Length)
+            {
+                waveEnemeis = null;
+                wave++;
+                //新しい敵を生成
+                if (wave < maxWave)
+                    StartCoroutine(Instance(wave));
+                //Waveの最後ならエンドフラグをtrueに
+                if (wave == maxWave)
+                    isEnd = true;
+            }
         }
     }
 
     //敵をパターンごとに生成する
-    public void Instance(int number)
+    public void InstancePatternEnemy(int number)
     {
         int length = instancePattern[number].transform.childCount;
         GameObject[] enemies = new GameObject[length];
@@ -53,9 +89,11 @@ public class EnemyManager : MonoBehaviour
             enemies[i].transform.position = instancePattern[number].GetChild(i).position;
             //enemy.transform.position = transform.position;
         }
+        waveEnemeis = enemies;
         StartCoroutine(FrameIn(enemies));
     }
 
+    //生成した敵を上からフレームインさせる
     private IEnumerator FrameIn(GameObject[] enemies)
     {
         int time = 20;
@@ -69,5 +107,12 @@ public class EnemyManager : MonoBehaviour
             }
             time--;
         }
+    }
+
+    //生成に少し間を置く
+    private IEnumerator Instance(int wave)
+    {
+        yield return new WaitForSeconds(3);
+        InstancePatternEnemy(wave);
     }
 }
