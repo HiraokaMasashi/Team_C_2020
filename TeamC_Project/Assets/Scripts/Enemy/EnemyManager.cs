705eee136f1ee31465e6ff4df96d053b21a0f5d4
後潟 斗志朗 <gensenya@gmail.com>
2020-10-27 21:38:45 +0900

    エネミーパターン処理

--- TeamC_Project/Assets/Scripts/Enemy/EnemyManager.cs
+++ TeamC_Project/Assets/Scripts/Enemy/EnemyManager.cs
@@ -28,10 +28,11 @@ public class EnemyManager : MonoBehaviour
         //経過時間が生成時間になったとき(生成時間より大きくなったとき)
         if (time > interval)
         {
+            Instance(0);
             //enemyをインスタンス化する(生成する)
-            GameObject enemy = Instantiate(enemyPrefab);
+            //   GameObject enemy = Instantiate(enemyPrefab);
             //生成した敵の座標を決定する(現状　このオブジェクトの座標に生成)
-            enemy.transform.position = transform.position;
+            //   enemy.transform.position = transform.position;
             //経過時間を初期化して再度時間計測を始める
             time = 0f;
         }
@@ -41,12 +42,32 @@ public class EnemyManager : MonoBehaviour
     public void Instance(int number)
     {
         int length = instancePattern[number].transform.childCount;
-        for(int i = 0; i < length; i++)
+        GameObject[] enemies = new GameObject[length];
+        for (int i = 0; i < length; i++)
         {
+            Debug.Log(length);
             //enemyをインスタンス化する(生成する)
             GameObject enemy = Instantiate(enemyPrefab);
+            enemies[i] = enemy;
             //生成した敵の座標を決定する
-            enemy.transform.position = instancePattern[number].GetChild(i).position;
+            enemies[i].transform.position = instancePattern[number].GetChild(i).position;
+            //enemy.transform.position = transform.position;
+        }
+        StartCoroutine(FrameIn(enemies));
+    }
+
+    private IEnumerator FrameIn(GameObject[] enemies)
+    {
+        int time = 20;
+        while (time > 0)
+        {
+            yield return null;
+            foreach (var e in enemies)
+            {
+                if (e != null)
+                    e.transform.position -= Vector3.up * 0.5f;
+            }
+            time--;
         }
     }
 }
