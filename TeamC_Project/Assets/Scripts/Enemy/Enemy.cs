123709308ff07ffa186b729f26bbe8bcee7fcaa6
誠司 <ms01ps02@gmail.com>
2020-10-27 22:18:34 +0900

    エネミーがポーズ中に動かないように修正

--- TeamC_Project/Assets/Scripts/Enemy/Enemy.cs
+++ TeamC_Project/Assets/Scripts/Enemy/Enemy.cs
@@ -4,36 +4,62 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    //HPスクリプトから参照するため
-    public int hp;
-    //
-    public float moveSpeed;
-    
-    private Rigidbody2D rb = null;
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private Vector3 destroyZone;
+
+    enum MoveMode
+    {
+        NORMAL,
+        INTERSEPTION,
+    }
+    private MoveMode currentMode;
 
     // Start is called before the first frame update
     void Start()
     {
-      
+        currentMode = MoveMode.NORMAL;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 自分自身のtransformを取得
-        Transform myTransform = this.transform;
+        //// 自分自身のtransformを取得
+        //Transform myTransform = this.transform;
 
-        //Translate関数を使用
-        //Translate(float x,float y,float z,Space relativeTo = Space.Self)
-        myTransform.Translate(0, moveSpeed, 0, Space.World);
+        ////Translate関数を使用
+        ////Translate(float x,float y,float z,Space relativeTo = Space.Self)
+        //myTransform.Translate(0, moveSpeed, 0, Space.World);
 
-        if (!GetComponent<Renderer>().isVisible)
+        switch (currentMode)
         {
-            Destroy(this.gameObject);
+            case MoveMode.NORMAL:
+                NormalMove();
+                break;
+
+            case MoveMode.INTERSEPTION:
+                break;
+
+            default:
+                break;
+        }
+
+        if (transform.position.y <= destroyZone.y)
+        {
+            Destroy(gameObject);
         }
     }
 
-    public void Move()
+    private void NormalMove()
+    {
+        Vector3 position = transform.position;
+        position += Vector3.down * moveSpeed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void InterseptionMove()
     {
 
     }
