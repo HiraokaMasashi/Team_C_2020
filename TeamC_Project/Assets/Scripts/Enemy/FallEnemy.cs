6b68a03cd76ed4474161e62f72b5d607a2cecb51
誠司 <ms01ps02@gmail.com>
2020-11-04 19:45:56 +0900

    落下してくるだけの敵の作成

---
+++ TeamC_Project/Assets/Scripts/Enemy/FallEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallEnemy : Enemy
+{
+    [SerializeField]
+    private float minSpeed = 1.0f;
+    [SerializeField]
+    private float maxSpeed = 3.0f;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        moveSpeed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        if (setupScrew.IsStan) return;
+
+        Move();
+        Death();
+    }
+
+    protected override void Move()
+    {
+        base.Move();
+    }
+}
