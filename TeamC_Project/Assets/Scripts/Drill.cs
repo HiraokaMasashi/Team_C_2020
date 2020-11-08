b0920ef163af6a3b4242ef17210bf7ccf24cf6cf
誠司 <ms01ps02@gmail.com>
2020-11-08 14:42:35 +0900

    ドリルギミックの作成

---
+++ TeamC_Project/Assets/Scripts/Drill.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drill : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1.0f;
+    [SerializeField]
+    private float destroyZoneY;
+
+    private bool isShoot;
+
+    private ScrewCollision screwCollision;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isShoot = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        DestroyDrill();
+    }
+
+    private void DestroyDrill()
+    {
+        if (transform.position.y >= destroyZoneY)
+            Destroy(gameObject);
+    }
+
+    private void Move()
+    {
+        if (!isShoot) return;
+
+        Vector3 position = transform.position;
+        position += Vector3.up * moveSpeed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Screw")
+        {
+            isShoot = true;
+        }
+
+        if(other.transform.tag == "Enemy" && isShoot)
+        {
+            screwCollision = GameObject.FindGameObjectWithTag("Screw").GetComponent<ScrewCollision>();
+            screwCollision.RemoveEnemy(other.gameObject);
+            other.GetComponent<Health>().HitDeath();
+        }
+    }
+}
